52a6dff7c7ad8936f067aa840879fd661a93ac2b
Pedro Cambra <pedro.cambra@gmail.com>
2014-03-26 12:43:15 +0100

    Updated slides for Drupal dev days 2014

--- slide_config.js
+++ slide_config.js
@@ -3,7 +3,7 @@ var SLIDE_CONFIG = {
   settings: {
     title: 'Drupal 8: Undercover initiatives',
     //subtitle: 'Subtitle Goes Here',
-    eventTitle: 'Drupal Camp London 2014',
+    eventTitle: 'Drupal Developer Days Szeged 2014',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
